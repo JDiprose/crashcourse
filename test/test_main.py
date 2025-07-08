1f74349144bec599721245969626a5f352154ed8
JDiprose <jon.diprose@well.ox.ac.uk>
2025-07-08 13:56:30 +0000

    flake8 improvements

--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,8 @@
 import sys
 from pathlib import Path
+import main
 sys.path.append(str(Path(__file__).parent.parent / "src"))
 
-import main
 
 def test_main_version():
     assert main._version == "0.0.1"
